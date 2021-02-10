04f832ba71d2d29fc9ca158c936f50fe9d7ebe26
mcorcias <mosescorcias@gmail.com>
2021-02-10 23:33:34 +0200

    installed the project - CLI - vue add vuetify - clean the unnecessary components

---
+++ playlist-project-vuetify/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+   <v-contect>
+     <router-view></router-view>
+   </v-contect>
+  </v-app>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+
+  components: {
+    
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
